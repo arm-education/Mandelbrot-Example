d3ccfdb3f20105a27b55f5431142c22552f7a1fa
Ubuntu <ubuntu@ip-10-248-213-124.eu-west-1.compute.internal>
2026-02-16 13:36:36 +0000

    updated default path in main

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
 
     Mandelbrot::Mandelbrot myplot(1920, 1080);
-    myplot.draw("./images/green.bmp", Mandelbrot::Mandelbrot::GREEN);
+    myplot.draw("/home/ubuntu/mandelbrot/images", Mandelbrot::Mandelbrot::GREEN);
 
     return 0;
 }
